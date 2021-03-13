28d4c79f361b7787f0807115866bc0ed878f6f11
Jasmine Woon <jxwoon@uw.edu>
2021-03-12 23:14:24 -0800

    fixed volume

--- volume.py
+++ volume.py
@@ -3,28 +3,26 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import math
 
-class Volume:
+def lower_volume():
+    # Get default audio device using PyCAW
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-    def lower_volume():
-        # Get default audio device using PyCAW
-        devices = AudioUtilities.GetSpeakers()
-        interface = devices.Activate(
-            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-        volume = cast(interface, POINTER(IAudioEndpointVolume))
+    # Get current volume 
+    currentVolumeDb = volume.GetMasterVolumeLevel()
+    volume.SetMasterVolumeLevel(currentVolumeDb - 6.0, None)
+    # NOTE: -6.0 dB = half volume !
 
-        # Get current volume 
-        currentVolumeDb = volume.GetMasterVolumeLevel()
-        volume.SetMasterVolumeLevel(currentVolumeDb - 6.0, None)
-        # NOTE: -6.0 dB = half volume !
+def raise_volume():
+    # Get default audio device using PyCAW
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-    def raise_volume():
-        # Get default audio device using PyCAW
-        devices = AudioUtilities.GetSpeakers()
-        interface = devices.Activate(
-            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-        volume = cast(interface, POINTER(IAudioEndpointVolume))
-
-        # Get current volume 
-        currentVolumeDb = volume.GetMasterVolumeLevel()
-        volume.SetMasterVolumeLevel(currentVolumeDb + 6.0, None)
-        # NOTE: -6.0 dB = half volume !
+    # Get current volume 
+    currentVolumeDb = volume.GetMasterVolumeLevel()
+    volume.SetMasterVolumeLevel(currentVolumeDb + 6.0, None)
+    # NOTE: -6.0 dB = half volume !
